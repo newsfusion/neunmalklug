e748c912c8d3c913776764386be5eca97fbf9c2d
Peter <Peter@PPC>
2012-04-10 17:39:13 +0200

    est1

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello Edy and Stefaan!');
+  response.send('Hello Edy and Stefaan! aaaa');
 });
  
 var port = process.env.PORT || 3000;
