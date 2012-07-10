fb2aae3f2c3bf57f13c9748f36b82c15cd045208
P. Schael <serv@newsfusion.de>
2012-07-10 05:06:29 -0400

    a

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello Edy and Stefaan! aaavvaaa');
+  response.send('Hello Edy and Stefaan! mit cloud9 ge2ändert!');
 });
  
-var port = process.env.PORT || 3000;
+var port = process.env.C9_PORT || process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
